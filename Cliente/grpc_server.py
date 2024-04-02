48860ec85014717b527b39f5ba18684156753550
DanyMelguizo <dmelguizor@eafit.edu.co>
2024-04-02 15:08:05 -0500

    'server'

--- Cliente/grpc_server.py
+++ Cliente/grpc_server.py
@@ -55,8 +55,13 @@ def get_file(name_file, ip):
     with grpc.insecure_channel(f'{ip}:{port}') as channel:
         stub = services_pb2_grpc.ServicesStub(channel)
         response = stub.SendNode(services_pb2.NameFile(name=name_file))
-
         nodes = response.nodes
+    
+        try:
+            node = json.loads(response.node)
+        except:
+            print("No se pudo transformar a json")
+
 
     print(f"Nodes: {nodes}")
 
