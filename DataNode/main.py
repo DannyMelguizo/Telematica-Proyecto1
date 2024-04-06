0f0227646adc3c323770dc4da44a2c0859c3abe8
DanyMelguizo <dmelguizor@eafit.edu.co>
2024-04-06 18:03:03 -0500

    replicacion de bloques

--- DataNode/main.py
+++ DataNode/main.py
@@ -71,8 +71,6 @@ def handle_client(client_socket, client_address):
         peers[1] = client_address[0]
     else:
         peers.append(client_address[0])
-    
-    print(peers)
     client_socket.close()
 
 def asign_node(file_name, block):
@@ -107,6 +105,7 @@ def validate_ip(ip):
 
 def get_peers():
     global peers
+    print(peers)
     return peers
 
 if __name__ == '__main__':
